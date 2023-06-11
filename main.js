4b33e914d5bd07612e78393decadf63b55715eb0
Fedor Sherbanyuk <fedor.sherbanyuk1@gmail.com>
2023-06-11 23:26:06 +0300

    add mobile version4

--- main.js
+++ main.js
@@ -41,6 +41,82 @@ if(animItems.length>0){
     },400);
 
 }
+let carousel = document.querySelector('#myCarousel');
+let carouselItems = carousel.querySelectorAll('.carousel-item');
+let windowWidth = window.innerWidth;
+let currentIndex = 0;
+let itemsPerSlide = (windowWidth < 780) ? 4 : 4;// Количество отображаемых слайдов на экране
+
+let totalItems = carouselItems.length;
+
+// Функция для переключения слайдов
+function switchSlide() {
+    for (let i = 0; i < carouselItems.length; i++) {
+        if (i >= currentIndex && i < currentIndex + itemsPerSlide) {
+            carouselItems[i].classList.add('active');
+        } else {
+            carouselItems[i].classList.remove('active');
+        }
+    }
+
+    currentIndex += itemsPerSlide;
+    if (currentIndex >= totalItems) {
+        currentIndex = 0;
+    }
+}
+
+// Периодический вызов функции для переключения слайдов
+setInterval(switchSlide, 800); // Интервал 0.8 секунды (800 миллисекунд)
+
+
+
+//
+// // Получение элементов слайдов и карусели
+// let carousel = document.getElementById('carousel');
+// let slides = carousel.getElementsByClassName('slide');
+//
+// // Переменные для отслеживания текущего слайда и таймера
+// let currentSlide = 0;
+// let timer;
+//
+// // Функция для переключения слайдов
+// function showSlide() {
+//     // Скрытие предыдущего слайда
+//     slides[currentSlide].style.display = 'none';
+//
+//     // Увеличение индекса текущего слайда
+//     currentSlide++;
+//
+//     // Если достигнут конец слайдов, переход к первому слайду
+//     if (currentSlide >= slides.length) {
+//         currentSlide = 0;
+//     }
+//
+//     // Показ текущего слайда
+//     slides[currentSlide].style.display = 'block';
+// }
+//
+// // Запуск карусели
+// function startCarousel() {
+//     // Показ первого слайда
+//     slides[currentSlide].style.display = 'block';
+//
+//     // Установка интервала
+//     timer = setInterval(showSlide, 800); // Интервал в миллисекундах (0.8 секунды)
+// }
+//
+// // Остановка карусели
+// function stopCarousel() {
+//     // Скрытие текущего слайда
+//     slides[currentSlide].style.display = 'none';
+//
+//     // Очистка интервала
+//     clearInterval(timer);
+// }
+//
+// // Запуск карусели при загрузке страницы
+// startCarousel();
+
 
 // <!--        <div class="m-5 col-sm-10 alert-primary d-flex justify-content-center">-->
 // <!--            <h2 class="photo_feature_text activeNew text"> АУДИТ</h2>-->
